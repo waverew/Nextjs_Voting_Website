6e5e8b25c7daacad9483d5cc645edb07726de74a
waverew <waverew@gmail.com>
2023-04-13 21:37:52 -0400

    working on login system

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -3,11 +3,7 @@
 import {db} from '../../firebase'
 import {ref, get, set} from 'firebase/database'
 export default async function handler(req, res) {
-  const re = new ref(db, "user")
-  set(re,{
-    "test":"succ"
-  })
+  const re = new ref(db, "user");
   const ge = await get(re);
-  console.log(ge.val());
   res.status(200).json({ name: 'John Doe' })
 }
--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,13 +1,23 @@
 import { db } from "../../firebase";
 import { ref, get, set } from "firebase/database";
 export default async function handler(req, res) {
-
-    console.log(req.body)
-const {username, password} = req.body
-    console.log(username, password)
-    
-    if(username == "bebra@g.com" && password == "bebra"){
+  const { username, password } = req.body;
+  const re = ref(db, "user");
+  const ge = await get(re);
+  console.log(typeof(ge.val().password));
+  console.log(password == ge.val().password)
+    Object.keys(ge).forEach(key => {
+        if(toString(ge.key) == password){
+            console.log("user ok");
+            Object.key(ge).forEach(k => {
+                if (toString(ge[k]) == username){
+                    console.log("succ");
+                }
+            })
+        }
+    });
+  /*if(username == "bebra@g.com" && password == "bebra"){
         res.redirect('../dashboard');
-    }
-    res.status(300).send("no");
+    }*/
+  res.status(300).send("no");
 }
