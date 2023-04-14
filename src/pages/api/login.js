9152ac3511a0a6657317382d0628f891d805a064
waverew <waverew@gmail.com>
2023-04-14 13:20:47 -0400

    login done

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -4,18 +4,12 @@ export default async function handler(req, res) {
   const { username, password } = req.body;
   const re = ref(db, "user");
   const ge = await get(re);
-  console.log(typeof(ge.val().password));
-  console.log(password == ge.val().password)
-    Object.keys(ge).forEach(key => {
-        if(toString(ge.key) == password){
-            console.log("user ok");
-            Object.key(ge).forEach(k => {
-                if (toString(ge[k]) == username){
-                    console.log("succ");
-                }
-            })
-        }
-    });
+  if (
+    Object.values(ge.val()).includes(username) &&
+    Object.values(ge.val()).includes(password)
+  ) {
+    res.redirect("../dashboard");
+  }
   /*if(username == "bebra@g.com" && password == "bebra"){
         res.redirect('../dashboard');
     }*/
