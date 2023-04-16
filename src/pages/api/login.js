af5d41873c9f92fa1363885004489ddce5f400d8
waverew <waverew@gmail.com>
2023-04-16 19:25:47 -0400

    minor

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -4,14 +4,13 @@ export default async function handler(req, res) {
   const { username, password } = await req.body;
   const re = ref(db, "user");
   const ge = await get(re);
-  
+
   // TODO: add routs protection, authentication and forgot password button
-  console.log(ge.val()[username]);
-  if (
-    Object.values(ge.val()).includes(username) &&
-    Object.values(ge.val()).includes(password)
-  ) {
-    res.status(200).redirect("../dashboard");
+  console.log(Object.keys(ge.val()).includes(username));
+  if (Object.keys(ge.val()).includes(username)) {
+    if (Object.values(ge.val()[username]).includes(password)) {
+      res.status(200).redirect("../dashboard");
+    }
   }
-  res.status(300).send("no");
+  else res.status(300).send("no");
 }
